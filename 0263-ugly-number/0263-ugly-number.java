24a41df2a00e7a6fc1a532603e3b07ec6042f1de
Prakash Selvadurai <prakashselvadurai26@gmail.com>
2026-07-22 21:48:18 +0530

    Time: 1 ms (98.98%), Space: 42.4 MB (70.24%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isUgly(int n) {
+        
+        if(n<=0){
+            return false;
+        }
+
+        for(int i=2;i<=5;i++){
+            if(i==4)
+            continue;
+            while(n%i==0){
+                n=n/i;
+            }
+        }
+         return n==1;
+    }
+}
