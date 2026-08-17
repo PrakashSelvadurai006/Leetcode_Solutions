6f4a0bdf5c742860a170f8bfe80b189e18fd598b
Prakash Selvadurai <prakashselvadurai26@gmail.com>
2026-08-17 21:09:07 +0530

    Time: 1 ms (20.31%), Space: 43.8 MB (68.08%) - LeetHub

---
+++ 3028-ant-on-the-boundary/3028-ant-on-the-boundary.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        int count=0;
+        int pos=0;
+        for(int i=0;i<nums.length;i++){
+            pos+=nums[i];
+            if(pos==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
